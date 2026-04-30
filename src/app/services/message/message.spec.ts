7bef177456aa795f5bb1f130421047ee53d4c7cd
Salsabeel <58127260+sals1995@users.noreply.github.com>
2026-04-30 12:54:01 +0300

    adding testing in files

--- src/app/services/message/message.spec.ts
+++ src/app/services/message/message.spec.ts
@@ -1,6 +1,20 @@
-
+import { MessageService } from './message.service';
 describe('message service:', () => {
-    it('dummy test', () => {
-    expect(true).toBeTruthy();
+  it('add function should add new msg', () => {
+    let service = new MessageService();
+    service.add('hello');
+
+    expect(service.messages()[0].message).toBe('hello');
+    expect(service.messages()).toHaveLength(1);
+  });
+  it('clear function should remove all messages', () => {
+    let service = new MessageService();
+
+    service.add('hello');
+    service.add('hello');
+
+    service.clear()
+
+    expect(service.messages()).toHaveLength(0)
   });
 });
