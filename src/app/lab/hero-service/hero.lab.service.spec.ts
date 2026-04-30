c4df5cd6963a77c8d3b9f83837f53657009326ef
Salsabeel <58127260+sals1995@users.noreply.github.com>
2026-04-30 13:13:45 +0300

    adding lab files

---
+++ src/app/lab/hero-service/hero.lab.service.spec.ts
@@ -0,0 +1,7 @@
+describe("hero service (for lab) http testing:", () => {
+    
+    it("should make a GET request to fetch hero by id and emit the returned hero", () => { })
+    it("should make a PUT request to update a hero and emit the updated hero", () => { 
+        
+    })
+})
